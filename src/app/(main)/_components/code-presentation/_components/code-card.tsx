1a6d6c5c97d50f904f17dccd42b07ce67925886f
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2025-01-15 21:04:56 +0100

    feat: add contition for mobile

--- src/app/(main)/_components/code-presentation/_components/code-card.tsx
+++ src/app/(main)/_components/code-presentation/_components/code-card.tsx
@@ -5,6 +5,7 @@ import { CardHeader } from "./card-header";
 import { useSettingsStore } from "@/zustand/useSettingsStore";
 import ExtendUILogo from "@/app/_components/logo";
 import { useAuthStore } from "@/zustand/useAuthStore";
+import FadeUp from "@/components/fadeup";
 
 type Props = {
   currentCode: string;
@@ -31,40 +32,48 @@ export const CodeCard = ({
   console.log(subscribed)
 
   return (
-    <div
-      className={cn(
-        "relative overflow-hidden bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500",
-        "transition-all duration-300 ease-in-out",
-      )}
-      style={{ background: background, padding: padding }}
-    >
-      {!subscribed && (
-        <div className="absolute bottom-2 right-2 text-white">
-          <ExtendUILogo />
-        </div>
-      )}
-      <div
-        className={cn("p-1 shadow-xl")}
-        style={{
-          background: themeBackground,
-          borderRadius: radius,
-        }}
-      >
-        <CardHeader
-          cardTheme={cardTheme}
-          themeBorder={themeBorder}
-          themeText={themeText}
-        />
+    <>
+      <div className="hidden sm:block">
+        <div
+          className={cn(
+            "relative overflow-hidden bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 transition-all duration-300 ease-in-out",
+          )}
+          style={{ background: background, padding: padding }}
+        >
+          {!subscribed && (
+            <div className="absolute bottom-2 right-2 text-white">
+              <ExtendUILogo />
+            </div>
+          )}
+          <div
+            className={cn("p-1 shadow-xl")}
+            style={{
+              background: themeBackground,
+              borderRadius: radius,
+            }}
+          >
+            <CardHeader
+              cardTheme={cardTheme}
+              themeBorder={themeBorder}
+              themeText={themeText}
+            />
 
-        <HighlightCode
-          theme={theme}
-          currentCode={currentCode}
-          language={language}
-          currentSlide={currentSlide}
-          diffMap={diffMap}
-        />
-        <div className="py-2" />
+            <HighlightCode
+              theme={theme}
+              currentCode={currentCode}
+              language={language}
+              currentSlide={currentSlide}
+              diffMap={diffMap}
+            />
+            <div className="py-2" />
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="block sm:hidden">
+        <p className="mx-2 my-6 max-w-2xl text-base text-center font-light tracking-tight dark:text-zinc-300">
+          Flip your phone to see preview card.
+        </p>
+      </div>
+    </>
   );
 };
