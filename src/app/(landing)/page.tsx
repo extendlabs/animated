6eaf93bfd64a7339c9fb62aed42cc8cdbab5cbba
rynkovski <118986014+rynkovski@users.noreply.github.com>
2025-03-18 13:28:25 +0100

    feat: update layout

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,18 +3,22 @@ import CTA from "./_components/cta";
 import { FeaturesCustomization, FeaturesTools } from "./_components/features";
 import { customization, features } from "@/constants/features";
 import FadeUp from "@/components/fadeup";
+import Customers from "./_components/customers";
 
 export default function LandingPage() {
   return (
     <main>
       <HeroSection />
-      <FadeUp delay={0.4} duration={0.8}>
+      <FadeUp delay={0.2} duration={1}>
+        <Customers />
+      </FadeUp>
+      <FadeUp delay={0.4} duration={1}>
         <FeaturesTools
           features={features}
           defaultVideo="https://xvylq80vkq.ufs.sh/f/d0hpkByvvVRPJ94VhKZdoSVxuY6UW8X2KsL4HanzvlmbEt0I"
         />
       </FadeUp>
-      <FadeUp delay={0.6} duration={0.8}>
+      <FadeUp delay={0.6} duration={1}>
         <FeaturesCustomization
           features={customization}
           defaultVideo="https://xvylq80vkq.ufs.sh/f/d0hpkByvvVRPJ94VhKZdoSVxuY6UW8X2KsL4HanzvlmbEt0I"
